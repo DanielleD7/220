76991381086557d7bde9bda27f8a4e3b7a182d90
DanielleD7 <dipacedd@g.cofc.edu>
2022-02-24 21:58:50 -0500

    Completed hw6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,42 +1,80 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Danielle Di Pace
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Formatting strings to print in different ways, encoding a message using unicode,
+and returning calculated values from parameters.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
 
 
 def cash_converter():
-    pass
+    integer = eval(input("Enter an integer: "))
+    print("That is ${:.2f}".format(integer))
 
 
 def encode():
-    pass
+    message = input("Enter a message: ")
+    key = eval(input("Enter a key: "))
+    code = ""
+
+    for letter in message:
+        code = code + chr((ord(letter) + key))
+
+    print(code)
 
 
 def sphere_area(radius):
-    pass
+    surface_area = 4 * math.pi * (radius ** 2)
+    return surface_area
 
 
 def sphere_volume(radius):
-    pass
+    volume = (4 / 3) * math.pi * (radius ** 3)
+    return volume
 
 
 def sum_n(number):
-    pass
+    total = 0
+    for num in range(1, number + 1):
+        total = total + num
+
+    return total
 
 
 def sum_n_cubes(number):
-    pass
+    total = 0
+    for num in range(1, number + 1):
+        total = total + (num ** 3)
+
+    return total
 
 
 def encode_better():
-    pass
+    message = input("Enter a message: ")
+    key = input("Enter a key: ")
+
+    key_length = len(key)
+    encoded_message = ""
+    count = 0
+
+    # Encoding Loop
+    for letter in message:
+        message_letter_code = (ord(letter) - 65)
+        key_letter_code = (ord(key[count % key_length]) - 65)
+
+        new_code = message_letter_code + key_letter_code
+        new_code = (new_code % 58) + 65
+
+        encoded_message = encoded_message + chr(new_code)
+
+        count = count + 1
+    # Encoding Loop END
+
+    print(encoded_message)
 
 
 if __name__ == '__main__':
