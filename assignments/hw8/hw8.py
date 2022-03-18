5085d0bac0b37a68a59275e169e211281f276bdd
DanielleD7 <dipacedd@g.cofc.edu>
2022-03-18 19:26:54 -0400

    Completed hw8

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -1,42 +1,67 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Danielle Di Pace
+hw8.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Various functions to manipulate integers, strings, and lists. These functions are then called
+by another function to calculate the sum of squares. Other functions using if statements to determine
+a person's weight class, if a year is a leap year, and if two circles overlap.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+import math
+from graphics import GraphWin, Point, Text, Circle
 
 
 def add_ten(nums):
-    pass
+    for index in range(len(nums)):
+        nums[index] = nums[index] + 10
 
 
 def square_each(nums):
-    pass
+    for index in range(len(nums)):
+        nums[index] = (nums[index] ** 2)
 
 
 def sum_list(nums):
-    pass
+    total = 0
+    for num in nums:
+        total = total + num
+    return total
 
 
 def to_numbers(nums):
-    pass
+    for index in range(len(nums)):
+        nums[index] = eval(nums[index])
 
 
 def sum_of_squares(nums):
-    pass
+    num_list = []
+    sum_sqr_list = []
+
+    for string_nums in nums:
+        num_list = string_nums.split(", ")
+        to_numbers(num_list)
+        square_each(num_list)
+        sum_sqr_list.append(sum_list(num_list))
+
+    return sum_sqr_list
 
 
 def starter(weight, wins):
-    pass
+    if 150 <= weight < 160 and wins >= 5:
+        return True
+    if weight > 199 or wins > 20:
+        return True
+    return False
 
 
 def leap_year(year):
-    pass
+    if year % 400 == 0:
+        return True
+    if (year % 4 == 0) and (not year % 100 == 0):
+        return True
+    return False
 
 
 def circle_overlap():
@@ -47,6 +72,7 @@ def circle_overlap():
     height = 10
     win.setCoords(0, 0, width, height)
 
+    # Circle One
     center = win.getMouse()
     circumference_point = win.getMouse()
     radius = math.sqrt(
@@ -55,11 +81,36 @@ def circle_overlap():
     circle_one.setFill("light blue")
     circle_one.draw(win)
 
+    # Circle Two
+    center = win.getMouse()
+    circumference_point = win.getMouse()
+    radius = math.sqrt(
+        (center.getX() - circumference_point.getX()) ** 2 + (center.getY() - circumference_point.getY()) ** 2)
+    circle_two = Circle(center, radius)
+    circle_two.setFill("light green")
+    circle_two.draw(win)
+
+    if did_overlap(circle_one, circle_two):
+        message = Text(Point(width / 2, height / 3), "The circles overlap")
+    else:
+        message = Text(Point(width / 2, height / 3), "The circles do not overlap")
+
+    close_message = Text(Point(width / 2, (height / 3) - 1), "Click again to close")
+
+    message.draw(win)
+    close_message.draw(win)
     win.getMouse()
 
 
 def did_overlap(circle_one, circle_two):
-    pass
+    distance = (circle_two.getCenter().getX() - circle_one.getCenter().getX()) ** 2
+    distance = distance + ((circle_two.getCenter().getY() - circle_one.getCenter().getY()) ** 2)
+    distance = math.sqrt(distance)
+    radius_sum = circle_one.getRadius() + circle_two.getRadius()
+
+    if distance <= radius_sum:
+        return True
+    return False
 
 
 if __name__ == '__main__':
